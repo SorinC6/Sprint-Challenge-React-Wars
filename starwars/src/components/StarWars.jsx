58246a0911f8622b8b0b510fdb87c68f9176ff39
Sorin Chis <kish.sorin@yahoo.com>
2019-02-15 11:12:39 +0200

    added style on the Components

--- starwars/src/components/StarWars.jsx
+++ starwars/src/components/StarWars.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import './StarWars.css'
 
 const StarWars = (props) => {
 
    return(
-      <div>
-         <h1>{props.dataList.name}</h1>
-         <h4>Gender: {props.dataList.gender}</h4>
-         <h4>Skin Color: {props.dataList.skin_color}</h4>
-         <h4>Hair Color: {props.dataList.hair_color}</h4>
-         <h4>Height: {props.dataList.height}</h4>
+      <div className='char-container'>
+         <h1 className='char-title'>{props.dataList.name}</h1>
+         <h4 className='item-style' >Gender: {props.dataList.gender}</h4>
+         <h4 className='item-style' >Skin Color: {props.dataList.skin_color}</h4>
+         <h4 className='item-style' >Hair Color: {props.dataList.hair_color}</h4>
+         <h4 className='item-style' >Height: {props.dataList.height}</h4>
       </div>
    )
 } 
--- starwars/src/components/StarWarsList.jsx
+++ starwars/src/components/StarWarsList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import StarWars from './StarWars';
+import './StarWars.css'
 
 const StarWarsList = (props) => {
    return(
-      <div>
+      <div className='list-container'>
          {
             props.dataList.map((character) =>(
                <StarWars
