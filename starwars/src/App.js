b8a39c900b9580f25796cdecb44fafab251e6ca7
Sorin Chis <kish.sorin@yahoo.com>
2019-02-15 11:38:46 +0200

    added functionality to Next and Previos Buttons

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import StarWarsList from './components/StarWarsList';
+import LoadPageButtons from './components/LoadPageButtons';
 import './components/StarWars.css'
+
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      starwarsChars: []
+      starwarsChars: [],
+      nextPage: null,
+      prevPage:null,
     };
   }
 
@@ -24,12 +28,28 @@ class App extends Component {
         return res.json();
       })
       .then(data => {
-        this.setState({ starwarsChars: data.results });
+         console.log(data);
+        this.setState({ 
+            starwarsChars: data.results, 
+            nextPage:data.next,
+            prevPage:data.previous,
+         });
       })
       .catch(err => {
         throw new Error(err);
       });
   };
+
+  prevPage = () => {
+     //console.log('prev');
+     if(this.state.prevPage)
+      this.getCharacters(this.state.prevPage)
+  }
+
+  nextPage = () => {
+     console.log('next');
+     this.getCharacters(this.state.nextPage);
+  }
 
   render() {
     return (
@@ -37,6 +57,10 @@ class App extends Component {
         <h2 className="Header">React  Wars</h2>
          <StarWarsList
             dataList={this.state.starwarsChars}
+         />
+         <LoadPageButtons
+            prevPage={this.prevPage}
+            nextPage={this.nextPage}
          />
       </div>
     );
--- starwars/src/components/LoadPageButtons.jsx
+++ starwars/src/components/LoadPageButtons.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const LoadMoreButtons = (props) => {
    return(
       <div className='btn-style'>
-         <button className='btn-style'>Previus Page</button>
-         <button className='btn-style'>Next Page</button>
+         <button className='btn-style' onClick={props.prevPage} >Previus Page</button>
+         <button className='btn-style' onClick={props.nextPage} >Next Page</button>
       </div>
    )
 }
