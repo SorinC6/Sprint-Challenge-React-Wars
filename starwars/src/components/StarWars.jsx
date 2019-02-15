256379d0c246073654e2eb39178d4af825fe5efa
Sorin Chis <kish.sorin@yahoo.com>
2019-02-15 10:41:47 +0200

    added StarWars component git status

---
+++ starwars/src/components/StarWars.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const StarWars = (props) => {
+
+   return(){
+      <div>
+         <h1>{props.dataList.name}</h1>
+         <h4>Gender: {props.dataList.gender}</h4>
+         <h4>Skin Color: {props.dataList.skin_color}</h4>
+         <h4>Hair Color: {props.dataList.hair_color}</h4>
+         <h4>Height: {props.dataList.height}</h4>
+      </div>
+   }
+} 
